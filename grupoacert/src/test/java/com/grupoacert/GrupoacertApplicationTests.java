1564cda1b9dd74498de9d6840287479342e0648c
rafaeldutra <rafaeldutra@parceirosbrk.com.br>
2023-06-15 20:30:23 -0300

    Cria projeto

---
+++ grupoacert/src/test/java/com/grupoacert/GrupoacertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupoacert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoacertApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
